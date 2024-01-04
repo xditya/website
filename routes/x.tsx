d423576b4af856ec285f74511d26470db72f4b1d
Roj <ez@roj.im>
2024-01-04 13:44:48 +0300

    Return examples

--- routes/x.tsx
+++ routes/x.tsx
@@ -1,30 +1,24 @@
 import { getDocs } from "../docs.ts";
-import { fixName, mapName } from "../misc.ts";
+import { fixName } from "../misc.ts";
+import { ClassMethodDef, DocNode, JsDocTagDoc } from "deno_doc/types.d.ts";
+
+function getV(v: DocNode | ClassMethodDef, a: string) {
+  return [fixName(v.name), {
+    link: `https://mtkruto.deno.dev/${a}/${fixName(v.name)}`,
+    example: (v.jsDoc?.tags?.find((v) =>
+      v.kind == "example"
+    ) as JsDocTagDoc).doc,
+  }] as const;
+}
 
 export default async function X() {
   const { types, methods, tlEnums, tlFunctions, tlTypes } = await getDocs();
   return Response.json(
-    types.map((v) => [v.name, `https://mtkruto.deno.dev/types/${v.name}`])
-      .concat(methods.map(
-        (v) => [v.name, `https://mtkruto.deno.dev/methods/${v.name}`],
-      ))
-      .concat(tlEnums.map(
-        (v) => [
-          fixName(v.name),
-          `https://mtkruto.deno.dev/tl/eunms/${fixName(v.name)}`,
-        ],
-      ))
-      .concat(tlTypes.map(
-        (v) => [
-          fixName(v.name),
-          `https://mtkruto.deno.dev/tl/types/${fixName(v.name)}`,
-        ],
-      ))
-      .concat(tlFunctions.map(
-        (v) => [
-          fixName(v.name),
-          `https://mtkruto.deno.dev/tl/functions/${fixName(v.name)}`,
-        ],
-      )),
+    types
+      .map((v) => getV(v, "types"))
+      .concat(methods.map((v) => getV(v, "methods")))
+      .concat(tlEnums.map((v) => getV(v, "tl/enums")))
+      .concat(tlTypes.map((v) => getV(v, "tl/types")))
+      .concat(tlFunctions.map((v) => getV(v, "tl/functions"))),
   );
 }
